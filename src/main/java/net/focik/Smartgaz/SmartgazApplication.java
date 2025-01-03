3ba8fb420498266f6381cc30721fe4aa320dca89
Tomek <tkrutowski@gmail.com>
2025-01-03 11:18:20 +0100

    init

---
+++ src/main/java/net/focik/Smartgaz/SmartgazApplication.java
@@ -0,0 +1,13 @@
+package net.focik.Smartgaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/focik/Smartgaz/SmartgazApplicationTests.java
@@ -0,0 +1,13 @@
+package net.focik.Smartgaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
